a9658c8b54c6c52ac40d5e1dc332215ad2db1e73
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-04-26 10:01:28 +0500

    new praduct

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import Footer from './page/Footer'
-import Header from './page/Header'
-import Main from './page/Main'
-import Heading from './page/companents/Heading.jsx'
+import Footer from "./page/Footer";
+import Header from "./page/Header";
+import Main from "./page/Main";
+import Heading from "./page/companents/Heading.jsx";
 function App() {
-
   return (
-    <div className='app'>
-<Header/>
-<Heading/>
-<Main/>
-<Footer/>
+    <div className="app">
+      <Header />
+      <Heading />
+      <Main />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/page/companents/Glassess.jsx
+++ src/page/companents/Glassess.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import glassess1 from "../../assets/images/0126756_nike-wmns-air-force-1-07-dd8959-100.webp";
 import glassess2 from "../../assets/images/0173433_nike-air-force-1-07-fz4617-001.webp";
-import glassess3 from "../../assets/images/0152890_nike-air-force-1-07-fq8201-100.webp";
+import glassess3 from "../../assets/images/0125847_nike-court-vision-lo-nn-dh2987-101.webp";
 export default function Glassess() {
   return (
     <div className="Glassess-all">
@@ -22,7 +22,7 @@ export default function Glassess() {
           <div className="Glassess-block-btn">
             <img src={glassess3} alt="" />
             <div className="Glassess-block-btn-text">
-           <p> Nike air force 5 </p>
+           <p> Nike Court Vision Lo Nn </p>
             </div>
           </div>
         </div>
