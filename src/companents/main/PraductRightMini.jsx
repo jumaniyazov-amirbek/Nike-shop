3fb42184c69b13480ba36cccc18422f2d39dd079
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-05-11 21:54:58 +0500

    format code

--- src/companents/main/PraductRightMini.jsx
+++ src/companents/main/PraductRightMini.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import glasses1 from "../../assets/GlassesImg/Frame (2).png";
 import glasses2 from "../../assets/GlassesImg/Frame (3).png";
 import glasses3 from "../../assets/GlassesImg/Frame (4).png";
@@ -10,182 +10,102 @@ import glasses8 from "../../assets/GlassesImg/Image.png";
 import { Link } from "react-router-dom";
 import heart from "../../assets/svg/heart.svg";
 export default function PraductRightMini() {
+  const [product, setProduct] = useState([
+    {
+      id: 1,
+      image: glasses1,
+      name: "Nike air force 6",
+      price: "$299,00",
+      sellPrice: "$400,00 ",
+      sale: "30% ",
+    },
+    {
+      id: 1,
+      image: glasses1,
+      name: "Nike air force 6",
+      price: "$299,00",
+      sellPrice: "$400,00 ",
+      sale: "30% ",
+    },
+    {
+      id: 1,
+      image: glasses1,
+      name: "Nike air force 6",
+      price: "$299,00",
+      sellPrice: "$400,00 ",
+      sale: "30% ",
+    },
+    {
+      id: 1,
+      image: glasses1,
+      name: "Nike air force 6",
+      price: "$299,00",
+      sellPrice: "$400,00 ",
+      sale: "30% ",
+    },
+    {
+      id: 1,
+      image: glasses1,
+      name: "Nike air force 6",
+      price: "$299,00",
+      sellPrice: "$400,00 ",
+      sale: "30% ",
+    },
+    {
+      id: 1,
+      image: glasses1,
+      name: "Nike air force 6",
+      price: "$299,00",
+      sellPrice: "$400,00 ",
+      sale: "30% ",
+    },
+    {
+        id: 1,
+        image: glasses1,
+        name: "Nike air force 6",
+        price: "$299,00",
+        sellPrice: "$400,00 ",
+        sale: "30% ",
+      },
+      {
+        id: 1,
+        image: glasses1,
+        name: "Nike air force 6",
+        price: "$299,00",
+        sellPrice: "$400,00 ",
+        sale: "30% ",
+      },
+  ]);
+
   return (
     <div className="Product-all-product-1-block-all">
-
-    <div className="Product-all-product-1-block">
-        <Link to="/WhenPressed">
-
-            <div className="Product-all-product-1-block-mini">
-                <div className="imgs">
-                    <img src={glasses1} alt="" />
-                </div>
-                <div className="Product-all-product-1-block-mini-text-all">
-                    <div className="Product-all-product-1-block-mini-text">
-                        <p>Nike air force 5</p>
-                    </div>
-
-                    <div className="Product-all-product-1-block-mini-price">
-                        <h2>$299,00</h2>
-
-                        <p>
-                            $400,00 / <span>30% Off</span>
-                        </p>
-                        <div className="heart-block">
-                            <img src={heart} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-        </Link>
-        <Link to="/WhenPressed">
-            <div className="Product-all-product-1-block-mini">
-                <div className="imgs">
-                    <img src={glasses2} alt="" />
-                </div>
-                <div className="Product-all-product-1-block-mini-text-all">
-                    <div className="Product-all-product-1-block-mini-text">
-                        <p>Nike air force 5</p>
-                    </div>
-
-                    <div className="Product-all-product-1-block-mini-price">
-                        <h2>$399,00</h2>
-                        <p>
-                            $499,00 / <span>10% Off</span>
-                        </p>
-                        <div className="heart-block">
-                            <img src={heart} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div></Link>
-        <Link to="/WhenPressed">    <div className="Product-all-product-1-block-mini">
-            <div className="imgs">
-                <img src={glasses3} alt="" />
-            </div>
-            <div className="Product-all-product-1-block-mini-text-all">
+      <div className="Product-all-product-1-block">
+        {product.map(item => (
+          <Link to="/WhenPressed">
+            <div className="Product-all-product-1-block-mini" key={item.id}>
+              <div className="imgs">
+                <img src={item.image} alt="" />
+              </div>
+              <div className="Product-all-product-1-block-mini-text-all">
                 <div className="Product-all-product-1-block-mini-text">
-                    <p>Nike Dunk Low</p>
+                  <p>{item.name}</p>
                 </div>
 
                 <div className="Product-all-product-1-block-mini-price">
-                    <h2>$350,00</h2>
-                    <p>
-                        $520,00 / <span>28% Off</span>
-                    </p>
-                    <div className="heart-block">
-                        <img src={heart} alt="" />
-                    </div>
-                </div>
-            </div>
-        </div> </Link>
-        <Link to="/WhenPressed">
-            <div className="Product-all-product-1-block-mini">
-                <div className="imgs">
-                    <img src={glasses4} alt="" />
-                </div>
-                <div className="Product-all-product-1-block-mini-text-all">
-                    <div className="Product-all-product-1-block-mini-text">
-                        <p>Nike Dunk Low</p>
-                    </div>
+                  <h2>{item.price}</h2>
 
-                    <div className="Product-all-product-1-block-mini-price">
-                        <h2>$380,00</h2>
-                        <p>
-                            $530,00 / <span>20% Off</span>
-                        </p>
-                        <div className="heart-block">
-                            <img src={heart} alt="" />
-                        </div>
-                    </div>
+                  <p>
+                    {item.sellPrice} / <span>{item.sale} Off</span>
+                  </p>
+                  <div className="heart-block">
+                    <img src={heart} alt="" />
+                  </div>
                 </div>
-            </div> </Link>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-    <div className="Product-all-product-1-block">
-        <Link to="/WhenPressed">   <div className="Product-all-product-1-block-mini">
-            <div className="imgs">
-                <img src={glasses5} alt="" />
-            </div>
-            <div className="Product-all-product-1-block-mini-text-all">
-                <div className="Product-all-product-1-block-mini-text">
-                    <p>Nike air force 5</p>
-                </div>
-
-                <div className="Product-all-product-1-block-mini-price">
-                    <h2>$100,000</h2>
-
-                    <p>
-                        $534,00 / <span>24% Off</span>
-                    </p>
-                    <div className="heart-block">
-                        <img src={heart} alt="" />
-                    </div>
-                </div>
-            </div>
-        </div></Link>
-        <Link to="/WhenPressed">    <div className="Product-all-product-1-block-mini">
-            <div className="imgs">
-                <img src={glasses6} alt="" />
-            </div>
-            <div className="Product-all-product-1-block-mini-text-all">
-                <div className="Product-all-product-1-block-mini-text">
-                    <p>Nike air force 5</p>
-                </div>
-
-                <div className="Product-all-product-1-block-mini-price">
-                    <h2>$199,00</h2>
-                    <p>
-                        $550,00 / <span>45% Off</span>
-                    </p>
-                    <div className="heart-block">
-                        <img src={heart} alt="" />
-                    </div>
-                </div>
-            </div>
-        </div></Link>
-        <Link to="/WhenPressed">
-            <div className="Product-all-product-1-block-mini">
-                <div className="imgs">
-                    <img src={glasses7} alt="" />
-                </div>
-                <div className="Product-all-product-1-block-mini-text-all">
-                    <div className="Product-all-product-1-block-mini-text">
-                        <p>Nike Dunk Low</p>
-                    </div>
-
-                    <div className="Product-all-product-1-block-mini-price">
-                        <h2>$599,00</h2>
-                        <p>
-                            $800,00 / <span>35% Off</span>
-                        </p>
-                        <div className="heart-block">
-                            <img src={heart} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div></Link>
-        <Link to="/WhenPressed">       <div className="Product-all-product-1-block-mini">
-            <div className="imgs">
-                <img src={glasses8} alt="" />
-            </div>
-            <div className="Product-all-product-1-block-mini-text-all">
-                <div className="Product-all-product-1-block-mini-text">
-                    <p>Nike Dunk Low</p>
-                </div>
-
-                <div className="Product-all-product-1-block-mini-price">
-                    <h2>$499,00</h2>
-                    <p>
-                        $510,00 / <span>5% Off</span>
-                    </p>
-                    <div className="heart-block">
-                        <img src={heart} alt="" />
-                    </div>
-                </div>
-            </div>
-        </div> </Link>
-    </div>
-</div>
-  )
+  );
 }
