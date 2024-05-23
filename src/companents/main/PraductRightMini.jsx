285a092661cbcbaabb2d5ea0a02c7507bb7e922e
Alibek <115139922+alibek-jumaniyazov@users.noreply.github.com>
2024-05-23 21:31:42 +0500

    add

--- src/companents/main/PraductRightMini.jsx
+++ src/companents/main/PraductRightMini.jsx
@@ -1,114 +1,60 @@
 import React, { useState } from "react";
-import glasses1 from "../../assets/GlassesImg/Frame (2).png";
-import glasses2 from "../../assets/GlassesImg/Frame (3).png";
-import glasses3 from "../../assets/GlassesImg/Frame (4).png";
-import glasses4 from "../../assets/GlassesImg/Frame (5).png";
-import glasses5 from "../../assets/GlassesImg/Image (1).png";
-import glasses6 from "../../assets/GlassesImg/Image (2).png";
-import glasses7 from "../../assets/GlassesImg/Image (3).png";
-import glasses8 from "../../assets/GlassesImg/Image.png";
+import axios from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import cart from '../../assets/svg/Cart.svg'
-import heart from "../../assets/svg/heart.svg";
-export default function PraductRightMini() {
-  const [product, setProduct] = useState([
-    {
-      id: 1,
-      image: glasses1,
-      name: "Nike air force 6",
-      price: "$299,00",
-      sellPrice: "$400,00 ",
-      sale: "30% ",
-    },
-    {
-      id: 2,
-      image: glasses2,
-      name: "Nike air force 6",
-      price: "$299,00",
-      sellPrice: "$400,00 ",
-      sale: "30% ",
-    },
-    {
-      id: 3,
-      image: glasses3,
-      name: "Nike air force 6",
-      price: "$299,00",
-      sellPrice: "$400,00 ",
-      sale: "30% ",
-    },
-    {
-      id: 4,
-      image: glasses4,
-      name: "Nike air force 6",
-      price: "$299,00",
-      sellPrice: "$400,00 ",
-      sale: "30% ",
-    },
-    {
-      id: 5,
-      image: glasses5,
-      name: "Nike air force 6",
-      price: "$299,00",
-      sellPrice: "$400,00 ",
-      sale: "30% ",
-    },
-    {
-      id: 6,
-      image: glasses6,
-      name: "Nike air force 6",
-      price: "$299,00",
-      sellPrice: "$400,00 ",
-      sale: "30% ",
-    },
-    {
-        id: 7,
-        image: glasses7,
-        name: "Nike air force 6",
-        price: "$299,00",
-        sellPrice: "$400,00 ",
-        sale: "30% ",
-      },
-      {
-        id: 8,
-        image: glasses8,
-        name: "Nike air force 6",
-        price: "$299,00",
-        sellPrice: "$400,00 ",
-        sale: "30% ",
-      },
-  ]);
+import cart from "../../assets/svg/Cart.svg";
+
+const PraductRightMini = () => {
+  const [data, setData] = useState([]);
+
+  const url = "https://128de5c23d00ef14.mokky.dev/praducts2";
+
+  useEffect(() => {
+    axios.get(url).then((res) => setData(res.data));
+  }, []);
+
+  const onAdd = (product) => {
+    addBasket(product);
+  };
 
   return (
-    <div className="Product-all-product-1-block-all">
-      <div className="Product-all-product-1-block">
-        {product.map(item => (
-          <Link key={item.id} to="/WhenPressed">
-            <div className="Product-all-product-1-block-mini" key={item.id}>
-              <div className="imgs">
-                <img src={item.image} alt="" />
-              </div>
-              <div className="Product-all-product-1-block-mini-text-all">
-                <div className="Product-all-product-1-block-mini-text">
-                  <p>{item.name}</p>
-                </div>
+    <div id="kartoshka" className="bg-[#F6F8F9]">
+      <div className="Product-all-product-1-block-all">
+        <div className="Product-all-product-1-block">
+          {data.map((product) => (
+            <div>
+              <Link to="/WhenPressed">
+                <div
+                  className="Product-all-product-1-block-mini"
+                  key={product.id}
+                >
+                  <img src={product.img} alt="" />
+                  <div className="Product-all-product-1-block-mini-text-all">
+                    <div className="Product-all-product-1-block-mini-text">
+                      <p>{product.name}</p>
+                    </div>
 
-                <div className="Product-all-product-1-block-mini-price">
-                  <h2>{item.price}</h2>
+                    <div className="Product-all-product-1-block-mini-price">
+                      <h2>{product.price}</h2>
 
-                  <p>
-                    {item.sellPrice} / <span>{item.sale} Off</span>
-                  </p>
-                  <Link to="/orders">
-                  <div className="heart-block">
-                    <img src={cart} alt="" />
+                      <p>
+                        {product.sellPrice} / <span>{product.sale} Off</span>
+                      </p>
+                      <Link to="/orders">
+                        <div className="heart-block">
+                          <img src={cart} alt="" />
+                        </div>
+                      </Link>
+                    </div>
                   </div>
-              </Link>
                 </div>
-              </div>
+              </Link>
             </div>
-          </Link>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default PraductRightMini;
